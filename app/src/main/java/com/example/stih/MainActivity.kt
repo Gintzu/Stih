2c18f07b611a8c79a8a61429c4dcf2fa0e39d3f5
Gintzu <omg80063@gmail.com>
2021-12-07 21:51:03 +0300

    test

--- app/src/main/java/com/example/stih/MainActivity.kt
+++ app/src/main/java/com/example/stih/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.stih
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Начинаем!", Toast.LENGTH_SHORT)
+        myToast.show()
+        val randomIntent = Intent(this, Page1::class.java)
+        startActivity(randomIntent)
     }
 }
--- app/src/main/java/com/example/stih/Page1.kt
+++ app/src/main/java/com/example/stih/Page1.kt
@@ -1,20 +1,13 @@
 package com.example.stih
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class Page1<View> : AppCompatActivity() {
+class Page1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page1)
     }
-    fun button(view: View) {
-        val myToast = Toast.makeText(this, "Начинаем!", Toast.LENGTH_SHORT)
-        myToast.show()
-        val randomIntent = Intent(this, Page1::class.java)
-        startActivity(randomIntent)
-    }
-
 }
