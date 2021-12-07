076cdddc8bc34be4ded03630eeddc2fc72cc108d
Gintzu <omg80063@gmail.com>
2021-12-07 16:06:07 +0300

    test

--- app/src/main/java/com/example/stih/Page1.kt
+++ app/src/main/java/com/example/stih/Page1.kt
@@ -1,11 +1,20 @@
 package com.example.stih
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class Page1 : AppCompatActivity() {
+class Page1<View> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page1)
     }
+    fun button(view: View) {
+        val myToast = Toast.makeText(this, "Начинаем!", Toast.LENGTH_SHORT)
+        myToast.show()
+        val randomIntent = Intent(this, Page1::class.java)
+        startActivity(randomIntent)
+    }
+
 }
